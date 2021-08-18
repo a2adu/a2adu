3e40eba686094446ce7bfde3bfbdfdb827de7fd1
a2adu <88275557+a2adu@users.noreply.github.com>
2021-08-18 11:20:13 +0300

    Add files via upload

--- uzt.lt/src/components/ContainerMenu.js
+++ uzt.lt/src/components/ContainerMenu.js
@@ -8,20 +8,22 @@ import pic6 from './images/6.png'
 import pic7 from './images/7.png' 
 import pic8 from './images/8.png' 
 import pic9 from './images/9.png' 
-
-{/* import command used to add container menu images  */}
-
-const ContainerMenu = () => {
+ 
+const ContainerMenu = ({picName, widthImg}) => {
     return(
 	<div className="elementor-widget-container">
 	   <div className="elementor-image-box-wrapper">
-		<figure className="elementor-image-box-img">
+		<figure className="elementor-image-box-img" style={{backgroundImage: `url(${picName})`, width: '100px', height: '20px'}} >
+			Registruotis
 		    <a href="https://uzt.lt/registracija-ut/" registracija="" className="no-lightbox">
-	    		<img width="768" height="769" src={pic1} 
-	    		className="elementor-animation-grow attachment-medium_large size-medium_large" 
-	    		alt="Registracija" 
-	    		srcSet={`${pic2} 768w, ${pic3} 150w, ${pic4} 300w, ${pic5} 1024w, ${pic6} 700w, ${pic7} 1044w sizes="(max-width: 768px) 100vw, 768px`}/>
+			<img src={picName} width="100" height="50" />
+	    		 
+	    		<img className="elementor-animation-grow attachment-medium_large size-medium_large" 
+	    		alt="Registracija"
+				/>    		
 	    	    </a>
+			 
+				{/*srcSet={`${pic2} 768w, ${pic3} 150w, ${pic4} 300w, ${pic5} 1024w, ${pic6} 700w, ${pic7} 1044w sizes="(max-width: 768px) 100vw, 768px`}/> */}
 		</figure>
 		<div className="elementor-image-box-content">
 			 <h3 className="elementor-image-box-title"><a href="https://uzt.lt/registracija-ut/" registracija="">Registracija</a></h3>
